45b2c2bcb7351e680fac31d318f0de1afab4ecee
ktallapakam <ktallapakam@gmail.com>
2025-04-01 10:48:00 +0530

    webhook - HelloController.java

--- src/main/java/com/test/hello/world/dockerdemo/controller/HelloController.java
+++ src/main/java/com/test/hello/world/dockerdemo/controller/HelloController.java
@@ -10,7 +10,9 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(@RequestParam() String name) {
-        //returns hello world as string.,,,,
+        /**
+            returns hello world as string.,,,,
+        **/
         return "<h1>Hello " + name+"....!</h1>";
     }
 }
