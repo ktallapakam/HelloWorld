31769f7cd0085f3b8c329a1cf3c3b1eef442ff5a
ktallapakam <ktallapakam@gmail.com>
2025-04-01 10:53:20 +0530

    Update DockerDemoApplication.java

--- src/main/java/com/test/hello/world/dockerdemo/DockerDemoApplication.java
+++ src/main/java/com/test/hello/world/dockerdemo/DockerDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DockerDemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello from main");
         SpringApplication.run(DockerDemoApplication.class, args);
     }
 
